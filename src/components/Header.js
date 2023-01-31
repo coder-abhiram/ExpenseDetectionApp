c13ed840030b2b6e0f22bed355d93cf9b07f0a72
coder-abhiram <118660648+coder-abhiram@users.noreply.github.com>
2023-02-01 01:10:06 +0530

    first expense commit

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+
+
+    <div className="container9">
+       <div class="wrapper">
+        <div class="bg"> Expense Tracker </div>
+        <div class="fg"> Expense Tracker </div>
+      </div>
+    </div>
+
+    
+    /* <div className="seven">
+       <h2>
+          Expense Tracker
+       </h2>
+    </div>
+     */
+  )
+}
