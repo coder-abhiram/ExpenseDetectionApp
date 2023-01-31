5e3f56c895e31088dad3e4cf292dbcb92bddee5c
coder-abhiram <118660648+coder-abhiram@users.noreply.github.com>
2023-02-01 01:22:25 +0530

    Updates

---
+++ precache-manifest.ca0f94e93751ecba86b8eeb53071ff31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ba3e18306585e02f0f2b33ab69922c",
+    "url": "/ExpenseDetectionApp/index.html"
+  },
+  {
+    "revision": "17154bfd3c97b9a60231",
+    "url": "/ExpenseDetectionApp/static/css/main.7976aea2.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/ExpenseDetectionApp/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ExpenseDetectionApp/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17154bfd3c97b9a60231",
+    "url": "/ExpenseDetectionApp/static/js/main.b038d644.chunk.js"
+  },
+  {
+    "revision": "eafe2c10a5affbd8c1eb",
+    "url": "/ExpenseDetectionApp/static/js/runtime-main.d2d02b70.js"
+  }
+]);
